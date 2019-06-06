a1f1c9c1fe6614785ca806a1c614872ff91275ad
ChristianBorn <c.born@hhu.de>
2019-06-06 21:49:47 +0200

    Final Setup.py

--- Code/Import/setup.py
+++ Code/Import/setup.py
@@ -1,4 +1,24 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
+import os
 
-setup(console=['main.py'])
+
+os.environ['TCL_LIBRARY'] = "D:\\ProgramData\\Anaconda3\\DLLs\\tcl86t.dll"
+os.environ['TK_LIBRARY'] = "D:\\ProgramData\\Anaconda3\\DLLs\\tk86t.dll"
+base = None
+
+executables = [Executable("main.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {
+        'packages': ['sqlite3', 'pandas', 'numpy'],
+    },
+}
+
+setup(
+    name = "<any name>",
+    options = options,
+    version = "1.0",
+    description = '<any description>',
+    executables = executables
+)
